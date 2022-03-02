f43c19194504bda6747ebbba2e0fe2a4160b324f
Rochelle Longoria <rochellelong95@gmail.com>
2022-03-01 18:36:37 -0700

    commit

---
+++ main.js
@@ -0,0 +1,11 @@
+    function onclick(liked) {
+        console.log("element clicked", element);
+}
+    function removeButton() {
+        var add = document.querySelector("#add")
+        add.remove();
+}
+
+function sayLogout(element) {
+    element.innerText="Log Out"
+}
